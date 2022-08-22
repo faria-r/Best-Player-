58eead53b6ef1d192fdf6581159dc997936703a0
faria-r <fariacse11@gmail.com>
2022-08-22 23:15:27 +0600

    players Cost Calculated

---
+++ js/others.js
@@ -0,0 +1,27 @@
+const selectButtonTwo = document.getElementById('sl-Two').addEventListener('click',function () {
+    setListItem('secondPlayer');
+    // event.selectButtonOne.disabled = true;
+})
+const selectButtonThree = document.getElementById('sl-three').addEventListener('click',function () {
+    setListItem('thirdPlayer');
+    // event.selectButtonOne.disabled = true;
+})
+const selectButtonFour = document.getElementById('sl-four').addEventListener('click',function () {
+    setListItem('fourthPlayer');
+    // event.selectButtonOne.disabled = true;
+})
+const selectButtonFive = document.getElementById('sl-five').addEventListener('click',function () {
+    setListItem('fifthPlayer');
+    // event.selectButtonOne.disabled = true;
+})
+const selectButtonsix = document.getElementById('sl-six').addEventListener('click',function () {
+    setListItem('sixPlayer');
+    // event.selectButtonOne.disabled = true;
+})
+
+// const setEventTwo = setEventListener('sl-Two');
+// setEventTwo = setListItem('secondPlayer')
+// const selectButtonOne = document.getElementById('sl-one').addEventListener('click',function () {
+//     setListItem('firstPlayer');
+//     // event.selectButtonOne.disabled = true;
+// })
--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,51 @@
-
-document.getElementById('btn-add-items').addEventListener('click',function () {
-    const listCOntainer = document.getElementById('li-container');
+//setting player name to the list container
+function setListItem(playerName){
+    const listContainer = document.getElementById('li-container');
     const li = document.createElement('li');
-    // li.innerText = 'brand new item';
-    li.classList.add('item');
-    listCOntainer.appendChild(li);
+    const PlayerNameElement = document.getElementById(playerName);
+    const liText = PlayerNameElement.innerText;
+    li.innerText = liText;
+    listContainer.appendChild(li);
+
+
+    // const listLength = document.getElementsById('li-container');
+    // const listNumber = listLength.document.getElementByTagName('li');
+  
+    // if(listNumber.length > 5){
+    //     alert('you can not add more the five player');
+    // }
+}
+//function to set eventlistener With buttons
+function setEventListener(buttonId,playerName){
+    document.getElementById(buttonId).addEventListener('click',function(event){
+        event.target.disabled = true;
+        event.target.style.backgroundColor = 'gray';
+        setListItem(playerName);
+        
+    })
+}
+//function to set value to an element
+function setTextElementValueById(elementId,value)
+{
+const elementField = document.getElementById(elementId);
+elementField.innerText = value;
+}
+setEventListener('sl-one','firstPlayer');
+setEventListener('sl-Two','secondPlayer');
+setEventListener('sl-three','thirdPlayer');
+setEventListener('sl-four','fourthPlayer');
+setEventListener('sl-five','fifthPlayer');
+setEventListener('sl-six','sixPlayer');
+
+
+//geeting the number of list elements in ordered list 
+document.getElementById('calcPlayerCost').addEventListener('click',function(){
+    const elements = document.getElementsByTagName('li');
+    const numberOfPlayers = elements.length;
+    const playerCostField = document.getElementById('playerCost');
+    const playerFieldString = playerCostField.value;
+    const playerCostAmount = parseInt(playerFieldString);
+    const totalPlayerCost =numberOfPlayers * playerCostAmount;
+    setTextElementValueById('PlayerExpense',totalPlayerCost);
+
 })
